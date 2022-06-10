babfb4b55bf0950c4a2d39efc2fc796ea25a13a6
Jeff Oliver <jeffoliver3384@gmail.com>
2022-06-10 16:38:30 -0400

    refactored mongo connection to use mongoose, built mongoose schemas for users and tours

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,10 +47,10 @@ const Home: NextPage<Props> = ({ tours }) => {
 export default Home;
 
 const getServerSideProps: GetServerSideProps = async () => {
-  const res: Response = await fetch("http://localhost:3000/api/tours");
+  // const res: Response = await fetch("http://localhost:3000/api/tours");
 
   // console.log(await res.json());
-  // const res: Response = await fetch("https://wanderlust-liart.vercel.app/api/tours");
+  const res: Response = await fetch("https://wanderlust-liart.vercel.app/api/tours");
 
   const tours: Tour[] = await res.json();
 
