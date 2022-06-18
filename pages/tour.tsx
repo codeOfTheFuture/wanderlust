d397a1eb07c6569bc5b0c4ad23a60163ae43f143
Jeff Oliver <jeffoliver3384@gmail.com>
2022-06-18 00:13:56 -0400

    debugging issue with getserversideprops in production

--- pages/tour.tsx
+++ pages/tour.tsx
@@ -21,35 +21,35 @@ const Tour: NextPage<Props> = ({ tours, tour, guide }) => {
   console.log(tour, guide, tours);
   return (
     <Layout>
-      <div className='flex flex-col justify-center items-center gap-5'>
+      {/* <div className='flex flex-col justify-center items-center gap-5'>
         <TourPageHeader backgroundImage={tour_photos[0]} title={title} />
         <TourDetails tour={tour} guide={guide} />
         <div className='w-1/2 h-[1px] mx-auto bg-black'></div>
         <RecommendedTours tours={tours} />
-      </div>
+      </div> */}
     </Layout>
   );
 };
 
 export default Tour;
 
-const getServerSideProps: GetServerSideProps = async context => {
-  const tour_id: string = context.query.tour as string;
+// const getServerSideProps: GetServerSideProps = async context => {
+//   const tour_id: string = context.query.tour as string;
 
-  const tour: Tour = await getTourById(tour_id);
+//   const tour: Tour = await getTourById(tour_id);
 
-  const [tours, guide]: [Tour[], User] = await Promise.all([
-    getAllTours(),
-    getUserById(tour.guide_id.toString()),
-  ]);
+//   const [tours, guide]: [Tour[], User] = await Promise.all([
+//     getAllTours(),
+//     getUserById(tour.guide_id.toString()),
+//   ]);
 
-  return {
-    props: {
-      tours: JSON.parse(JSON.stringify(tours)),
-      tour: JSON.parse(JSON.stringify(tour)),
-      guide: JSON.parse(JSON.stringify(guide)),
-    },
-  };
-};
+//   return {
+//     props: {
+//       tours: JSON.parse(JSON.stringify(tours)),
+//       tour: JSON.parse(JSON.stringify(tour)),
+//       guide: JSON.parse(JSON.stringify(guide)),
+//     },
+//   };
+// };
 
-export { getServerSideProps };
+// export { getServerSideProps };
