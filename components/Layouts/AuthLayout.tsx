fa96c3b06e1e8a2896f77c95e2d782193bed95c5
Jeff Oliver <jeffoliver3384@gmail.com>
2022-11-21 14:35:45 -0500

    navbar

--- components/Layouts/AuthLayout.tsx
+++ components/Layouts/AuthLayout.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import LoginImage from "../auth-form/LoginImage";
-import Navbar from "../nav/Navbar";
+// import Navbar from "../nav/Navbar";
 import { Toaster } from "react-hot-toast";
 
 interface Props {
@@ -12,7 +12,7 @@ const AuthLayout: FC<Props> = ({ children }) => {
     <div className="w-full h-screen bg-login-blurred bg-cover lg:bg-center lg:bg-no-repeat relative">
       <Toaster />
       <div className="absolute w-full h-full bg-black opacity-30"></div>
-      <Navbar scrollPosition={0} />
+      {/* <Navbar scrollPosition={0} /> */}
       <main className="flex flex-col lg:flex-row justify-center items-center w-full lg:w-1/2 h-full mx-auto">
         <LoginImage />
         {children}
