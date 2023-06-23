48d490adf6806057ac3983d40afab84d6569113b
Jeff Oliver <jeffoliver3384@gmail.com>
2023-06-23 02:37:55 -0400

    Changed font size of main heading on home page

--- components/tour-cards/TourCard.tsx
+++ components/tour-cards/TourCard.tsx
@@ -4,150 +4,147 @@ import { NextRouter, useRouter } from "next/router";
 import { Tour } from "../../types/typings";
 import { HeartIcon, PencilAltIcon } from "@heroicons/react/solid";
 import {
-  addTourToFavorites,
-  removeTourFromFavorites,
-  selectUser,
+	addTourToFavorites,
+	removeTourFromFavorites,
+	selectUser,
 } from "../../store/slices/userSlice";
 import { useAppDispatch, useAppSelector } from "../../store";
 import { Rating } from "react-simple-star-rating";
 import toast from "react-hot-toast";
 
 interface Props {
-  tour: Tour;
+	tour: Tour;
 }
 
 const TourCard: FC<Props> = ({ tour }) => {
-  const { _id, guideId, title, tourPhotos, price } = tour;
+	const { _id, guideId, title, tourPhotos, price } = tour;
 
-  const user = useAppSelector(selectUser);
+	const user = useAppSelector(selectUser);
 
-  const router: NextRouter = useRouter();
-  const dispatch = useAppDispatch();
+	const router: NextRouter = useRouter();
+	const dispatch = useAppDispatch();
 
-  const tourPageUrl = `/tour?tour=${_id}`;
-  const updateTourUrl = `/update-tour?tour=${_id}`;
+	const tourPageUrl = `/tour?tour=${_id}`;
+	const updateTourUrl = `/update-tour?tour=${_id}`;
 
-  const handleClick = () => {
-    if (
-      user?._id.toString() === guideId &&
-      router.pathname === "/offered-tours"
-    ) {
-      router.push(updateTourUrl);
-    } else {
-      router.push(tourPageUrl);
-    }
-  };
+	const handleClick = () => {
+		if (user?._id.toString() === guideId && router.pathname === "/offered-tours") {
+			router.push(updateTourUrl);
+		} else {
+			router.push(tourPageUrl);
+		}
+	};
 
-  const tourFavorite =
-    user?.favoriteTours.find((t: Tour) => t._id === tour._id) != null;
+	const tourFavorite = user?.favoriteTours.find((t: Tour) => t._id === tour._id) != null;
 
-  return (
-    <div
-      className="relative flex flex-col justify-end items-center w-full h-[300px] bg-slate-200 cursor-pointer border-2 border-slate-700 shadow-lg hover:shadow-xl rounded-sm px-2"
-      onClick={handleClick}>
-      {tour.tourPhotos.length > 0 && (
-        <Image
-          src={tourPhotos[0].secure_url}
-          alt={title}
-          layout="fill"
-          objectFit="cover"
-          className="absolute"
-        />
-      )}
+	return (
+		<div
+			className="relative flex flex-col justify-end items-center w-full h-[300px] bg-slate-200 cursor-pointer border-2 border-slate-700 shadow-lg hover:shadow-xl rounded-sm px-2"
+			onClick={handleClick}
+		>
+			{tour.tourPhotos.length > 0 && (
+				<Image
+					src={tourPhotos[0].secure_url}
+					alt={title}
+					layout="fill"
+					objectFit="cover"
+					className="absolute"
+				/>
+			)}
 
-      <div
-        className={`${
-          tour?.rating ? "justify-between" : "justify-end"
-        } flex items-center absolute top-2 w-full px-4`}>
-        {tour?.rating ? (
-          <div className="flex justify-center items-center w-28 h-8 bg-black rounded-full bg-opacity-60">
-            <Rating
-              size={20}
-              readonly
-              initialValue={tour.rating.ratingValue}
-              SVGclassName="inline-block"
-              className="mb-1"
-              allowFraction
-            />
-          </div>
-        ) : null}
+			<div
+				className={`${
+					tour?.rating ? "justify-between" : "justify-end"
+				} flex items-center absolute top-2 w-full px-4`}
+			>
+				{tour?.rating ? (
+					<div className="flex justify-center items-center w-28 h-8 bg-black rounded-full bg-opacity-60">
+						<Rating
+							size={20}
+							readonly
+							initialValue={tour.rating.ratingValue}
+							SVGclassName="inline-block"
+							className="mb-1"
+							allowFraction
+						/>
+					</div>
+				) : null}
 
-        {user?._id.toString() === guideId &&
-        router.pathname === "/offered-tours" ? (
-          <PencilAltIcon className="w-10 text-black opacity-90 hover:scale-125 transition-all duration-300 ease-in-out" />
-        ) : (
-          <HeartIcon
-            className={`${user ? "block" : "hidden"} ${
-              tourFavorite ? "text-error-color" : "text-black opacity-60"
-            } w-10  opacity-90 hover:scale-125 transition-all duration-300 ease-in-out`}
-            onClick={e => {
-              e.stopPropagation();
-              if (tourFavorite) {
-                const promise = dispatch(
-                  removeTourFromFavorites({
-                    userId: user._id.toString(),
-                    tourId: tour._id.toString(),
-                  })
-                );
+				{user?._id.toString() === guideId && router.pathname === "/offered-tours" ? (
+					<PencilAltIcon className="w-10 text-black opacity-90 hover:scale-125 transition-all duration-300 ease-in-out" />
+				) : (
+					<HeartIcon
+						className={`${user ? "block" : "hidden"} ${
+							tourFavorite ? "text-error-color" : "text-black opacity-60"
+						} w-10  opacity-90 hover:scale-125 transition-all duration-300 ease-in-out`}
+						onClick={e => {
+							e.stopPropagation();
+							if (tourFavorite) {
+								const promise = dispatch(
+									removeTourFromFavorites({
+										userId: user._id.toString(),
+										tourId: tour._id.toString(),
+									})
+								);
 
-                toast.promise(
-                  promise,
-                  {
-                    loading: null,
-                    success: "Removed from favorites!",
-                    error: "Error removing from favorites",
-                  },
-                  {
-                    loading: {
-                      duration: 0,
-                    },
-                    success: {
-                      duration: 2000,
-                    },
-                    error: {
-                      duration: 2000,
-                    },
-                  }
-                );
-              } else {
-                const promise = dispatch(
-                  addTourToFavorites({
-                    userId: user?._id.toString() as string,
-                    tourId: tour._id.toString(),
-                  })
-                );
+								toast.promise(
+									promise,
+									{
+										loading: null,
+										success: "Removed from favorites!",
+										error: "Error removing from favorites",
+									},
+									{
+										loading: {
+											duration: 0,
+										},
+										success: {
+											duration: 2000,
+										},
+										error: {
+											duration: 2000,
+										},
+									}
+								);
+							} else {
+								const promise = dispatch(
+									addTourToFavorites({
+										userId: user?._id.toString() as string,
+										tourId: tour._id.toString(),
+									})
+								);
 
-                toast.promise(
-                  promise,
-                  {
-                    loading: null,
-                    success: "Added to favorites!",
-                    error: "Error adding to favorites",
-                  },
-                  {
-                    loading: {
-                      duration: 0,
-                    },
-                    success: {
-                      duration: 2000,
-                    },
-                    error: {
-                      duration: 2000,
-                    },
-                  }
-                );
-              }
-            }}
-          />
-        )}
-      </div>
+								toast.promise(
+									promise,
+									{
+										loading: null,
+										success: "Added to favorites!",
+										error: "Error adding to favorites",
+									},
+									{
+										loading: {
+											duration: 0,
+										},
+										success: {
+											duration: 2000,
+										},
+										error: {
+											duration: 2000,
+										},
+									}
+								);
+							}
+						}}
+					/>
+				)}
+			</div>
 
-      <div className="flex flex-col items-start w-full mt-4 p-2 z-10 bg-slate-200">
-        <h2 className="font-semibold">{title}</h2>
-        <p>${price} per person</p>
-      </div>
-    </div>
-  );
+			<div className="flex flex-col items-start w-full mt-4 p-2 z-10 bg-slate-200">
+				<h2 className="font-semibold">{title}</h2>
+				<p>${price} per person</p>
+			</div>
+		</div>
+	);
 };
 
 export default TourCard;
--- components/tour-cards/TourCards.tsx
+++ components/tour-cards/TourCards.tsx
@@ -8,52 +8,51 @@ import ReactLoading from "react-loading";
 import Pagination from "./Pagination";
 
 interface Props {
-  loading: boolean;
+	loading: boolean;
 }
 
 const TourCards: FC<Props> = ({ loading }) => {
-  const router = useRouter();
+	const router = useRouter();
 
-  const toursResults = useAppSelector(selectTours) as TourResults;
-  const status = useAppSelector(getToursStatus);
+	const toursResults = useAppSelector(selectTours) as TourResults;
+	const status = useAppSelector(getToursStatus);
 
-  return (
-    <div
-      className={`${
-        router.pathname === "/search" &&
-        "justify-between h-[100vh] p-4 mt-20 lg:mt-0"
-      } flex flex-col w-11/12 sm:w-5/6 lg:w-full mx-auto bg-white relative ${
-        (loading || status === "loading") && "overflow-hidden"
-      }`}>
-      {(loading || status === "loading") && (
-        <div className="w-full absolute top-36 flex justify-center items-center z-50">
-          <div className="flex justify-center items-center w-40 bg-white rounded-2xl">
-            <ReactLoading
-              type={"bars"}
-              width={100}
-              height={100}
-              color="#2196f3"
-              className="bg-transparent z-50"
-            />
-          </div>
-        </div>
-      )}
-      <div
-        className={`grid ${
-          router.pathname === "/search"
-            ? "lg:grid-cols-2 mt-16"
-            : "lg:grid-cols-3 xl:grid-cols-4 xl:h-[632px]"
-        } grid-cols-1 md:grid-cols-2 justify-start items-start gap-6 w-full ${
-          (status === "loading" || loading) && "opacity-50"
-        } `}>
-        {toursResults?.results?.map(tour => (
-          <TourCard key={tour._id.toString()} tour={tour} />
-        ))}
-      </div>
+	return (
+		<div
+			className={`${
+				router.pathname === "/search" && "justify-between h-[100vh] p-4 mt-20 lg:mt-0"
+			} flex flex-col w-11/12 sm:w-5/6 lg:w-full mx-auto bg-white relative ${
+				(loading || status === "loading") && "overflow-hidden"
+			}`}
+		>
+			{(loading || status === "loading") && (
+				<div className="w-full absolute top-36 flex justify-center items-center z-50">
+					<div className="flex justify-center items-center w-40 bg-white rounded-2xl">
+						<ReactLoading
+							type={"bars"}
+							width={100}
+							height={100}
+							color="#2196f3"
+							className="bg-transparent z-50"
+						/>
+					</div>
+				</div>
+			)}
+			<div
+				className={`grid ${
+					router.pathname === "/search" ? "lg:grid-cols-2 mt-16" : "lg:grid-cols-3 xl:grid-cols-4"
+				} grid-cols-1 md:grid-cols-2 justify-start items-start gap-6 w-full ${
+					(status === "loading" || loading) && "opacity-50"
+				} `}
+			>
+				{toursResults?.results?.map(tour => (
+					<TourCard key={tour._id.toString()} tour={tour} />
+				))}
+			</div>
 
-      {toursResults.results.length > 0 && <Pagination />}
-    </div>
-  );
+			{toursResults.results.length > 0 && <Pagination />}
+		</div>
+	);
 };
 
 export default TourCards;
